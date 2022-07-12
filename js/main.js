1a6626db286e7a41babc78f5539236f32b718bdd
Nacho Morales <nacho.morales5@gmail.com>
2022-07-12 20:46:40 -0300

    Desafio N°3

--- js/main.js
+++ js/main.js
@@ -47,6 +47,7 @@ for (i = 1; i <= cant; i++) {
 -Incorporar lo ejercitado en las clases anteriores, algoritmo condicional y algoritmo con ciclo.
 -Utilizar funciones para realizar esas operaciones */
 
+/*
 alert("Bienvenido al sistema CDN (Calculo de notas)")
 
 let cant, i;
@@ -110,4 +111,109 @@ function informe() {
     console.log("Exposicion: " + exposicion);
     console.log("Parcial: " + parcial);
     console.log("Nota final: " + nf);
+} 
+*/
+
+//Desafio JavaScript N° 3
+
+/*Traslada al proyecto integrador el concepto de objetos, visto en la clase de hoy. A partir de los ejemplos mostrados la primera clase, y en función del tipo de simulador que hayas elegido, deberás:
+-Incorporar al menos un Array en tu proyecto.
+-Utilizar algunos de los métodos o propiedades vistos en la clase */
+
+alert("Bienvenido al sistema CDN (Calculo de notas)")
+
+let cant, i;
+let integrador, exposicion, parcial, nf, n1, n2, n3, nombre;
+
+cant = prompt("Porfavor ingrese la cantidad de alumnos:");
+
+cicloDeNotas();
+
+alert("Gracias por usar el sistema CDN")
+
+function cicloDeNotas() {
+    for (i = 1; i <= cant; i++) {
+
+        nombre = prompt("Ingrese el nombre y apellido del alumno " + i);
+        alert("Ingrese las notas de " + nombre);
+
+        integrador = prompt("Nota de integrador: ");
+        exposicion = prompt("Nota de exposicion: ");
+        parcial = prompt("Nota de parcial: ");
+
+        n1 = notaIntegrador();
+        n2 = notaExposicion();
+        n3 = notaParcial();
+
+        nf = n1 + n2 + n3;
+
+        if (nf < 6.5) {
+            aprobado();
+        } else {
+            desaprobado();
+        }
+
+        informe();
+    }
+}
+
+function notaIntegrador() {
+    return (35 * integrador) / 100;
+}
+
+function notaExposicion() {
+    return (25 * exposicion) / 100;
+}
+
+function notaParcial() {
+    return (40 * parcial) / 100;
+}
+
+function aprobado() {
+    alert(`El estudiante ${nombre} REPRUEBA el curso, nota del integrador: ${integrador}, nota de exposicion: ${exposicion}, nota de parcial: ${parcial}`);
+}
+
+function desaprobado() {
+    alert(`El estudiante ${nombre} APRUEBA el curso, nota del integrador: ${integrador}, nota de exposicion: ${exposicion}, nota de parcial: ${parcial}`);
+}
+
+function informe() {
+    let informe = [nombre, integrador, exposicion, parcial, nf];
+
+    if (nf < 6.5) {
+        let resp = prompt(`¿Desea agregar una nota para salvar el promedio de ${nombre}? S/N`)
+
+        resp = resp.toUpperCase();
+
+        if (resp == "S") {
+            let nuevaNota = parseFloat(prompt("¿Que nota salvara este promedio?"));
+            informe.push(nuevaNota);
+
+            let nuevoPromedio = (nuevaNota + nf) / 2;
+            informe.push(nuevoPromedio);
+
+            console.log(`Nombre del alumno: ${informe[0]}`);
+            console.log(`Nota de Integrador: ${informe[1]}`);
+            console.log(`Nota de Exposicion: ${informe[2]}`);
+            console.log(`Nota de Parcial: ${informe[3]}`);
+            console.log(`Nota de trabajo extra: ${informe[5]}`)
+            console.log(`Promedio final: ${informe[6]}`)
+            console.log(" ");
+        } else {
+            console.log(`Nombre del alumno: ${informe[0]}`);
+            console.log(`Nota de Integrador: ${informe[1]}`);
+            console.log(`Nota de Exposicion: ${informe[2]}`);
+            console.log(`Nota de Parcial: ${informe[3]}`);
+            console.log(`Promedio final: ${informe[4]}`)
+            console.log(" ");
+        }
+    } else {
+        console.log(`Nombre del alumno: ${informe[0]}`);
+        console.log(`Nota de Integrador: ${informe[1]}`);
+        console.log(`Nota de Exposicion: ${informe[2]}`);
+        console.log(`Nota de Parcial: ${informe[3]}`);
+        console.log(`Promedio final: ${informe[4]}`)
+        console.log(" ");
+    }
+    return;
 }
