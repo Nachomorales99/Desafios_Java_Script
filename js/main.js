f46f485b45856439f83cf3cfda9d7bd60ccf9a6c
Nacho Morales <nacho.morales5@gmail.com>
2022-07-12 20:48:14 -0300

    Modificacion

--- js/main.js
+++ js/main.js
@@ -116,7 +116,7 @@ function informe() {
 
 //Desafio JavaScript N° 3
 
-/*Traslada al proyecto integrador el concepto de objetos, visto en la clase de hoy. A partir de los ejemplos mostrados la primera clase, y en función del tipo de simulador que hayas elegido, deberás:
+/*A partir de los ejemplos mostrados la primera clase, y en función del tipo de simulador que hayas elegido, deberás:
 -Incorporar al menos un Array en tu proyecto.
 -Utilizar algunos de los métodos o propiedades vistos en la clase */
 
